afc9d90de442f886b2f147615e7eca756f321b8f
Carlos Araújo da Costa <stevenndev8@gmail.com>
2025-02-09 06:14:03 -0300

    Aula02.java

--- Lessons/Aula02.java
+++ Lessons/Aula02.java
@@ -4,7 +4,7 @@ public class Aula02 {
     public static void main(String[] args) {
         // Existem 8 tipos primitivos em Java
         // int, float, boolean, char, long, double, byte, short
-        // Para iniciar uma variável, sem iniciamos pelo tipo dela
+        // Para iniciar uma variável, colocamos o tipo dela no início.
         int age = 19;
         System.out.println("Eu tenho " + age + " anos de idade");
     }
